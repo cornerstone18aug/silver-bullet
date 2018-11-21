e6b347f4b2798e3fc8a3d85a335219b076527a02
nator333 <m.nakamata35@gmail.com>
2018-11-20 16:08:49 -0800

    Add accessor method and String constant

--- src/main/java/ca/ciccc/silverBullet/utils/ConstUtil.java
+++ src/main/java/ca/ciccc/silverBullet/utils/ConstUtil.java
@@ -5,14 +5,19 @@ public class ConstUtil {
   private ConstUtil() {
   }
 
-  enum DisplaySizeEnum {
+  public enum DisplaySizeEnum {
     EXTERNAL_FRAME_WIDTH(1050),
     EXTERNAL_FRAME_HEIGHT(600);
 
-    int size;
-    DisplaySizeEnum(int size) {
+    double size;
+    DisplaySizeEnum(double size) {
       this.size = size;
     }
+    public double getSize(){
+      return this.size;
+    };
   }
+
+  public static final String APP_NAME = "SILVER BULLET";
 
 }
