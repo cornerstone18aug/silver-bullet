6b4e2e05fc7b1d97b821740c4eaa7f5ce270109f
Hao-Tse <maxwuhz@gmail.com>
2018-11-21 13:44:55 -0800

    Create Sound FIle input method

--- src/main/java/ca/ciccc/silverBullet/SilverBulletApplication.java
+++ src/main/java/ca/ciccc/silverBullet/SilverBulletApplication.java
@@ -3,14 +3,18 @@ package ca.ciccc.silverBullet;
 import ca.ciccc.silverBullet.gameplayEnums.PlayerAction;
 import ca.ciccc.silverBullet.gridElements.GridBoard;
 import javafx.animation.AnimationTimer;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 import java.awt.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+import java.net.URL;
 
 import java.util.Collections;
 import java.util.List;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ca.ciccc.silverBullet {
     requires javafx.graphics;
     requires javafx.controls;
     requires java.desktop;
+    requires javafx.media;
 
     opens ca.ciccc.silverBullet to javafx.graphics;
 }
