ebb733a960f89b89ba3df983f5d4daa93ca1f66f
Unknown <juanjvalenciagd@gmail.com>
2018-11-23 15:14:54 -0800

    Moved everything from application into a new Pane

--- src/main/java/ca/ciccc/silverBullet/SilverBulletApplication.java
+++ src/main/java/ca/ciccc/silverBullet/SilverBulletApplication.java
@@ -1,6 +1,7 @@
 package ca.ciccc.silverBullet;
 
 import ca.ciccc.silverBullet.enums.gameplay.PlayerAction;
+import ca.ciccc.silverBullet.gameBoard.GameScene;
 import ca.ciccc.silverBullet.gameBoard.GridBoard;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -22,61 +23,21 @@ public class SilverBulletApplication extends Application {
 
 
   private Pane root = new Pane();
+  private GameScene game = new GameScene();
   List<Node> children = Collections.synchronizedList(this.root.getChildren());
   Stage primaryStage;
   AnimationTimer timer;
 
-  GridBoard gameBoard;
-  Player testFirstPlayer;
-  Player testSecondPlayer;
-  double t = 0;
-  boolean isExecuting;
-  int currentActionNumber = 0;
-  int controllingPlayer = 0;
+
 
   @Override
   public void start(Stage stage) {
     this.primaryStage = stage;
+
     Scene scene = new Scene(this.createContent());
+
     scene.setOnKeyPressed(e -> {
-      switch (e.getCode()) {
-        case Q:
-          if (controllingPlayer == 0){
-            testFirstPlayer.addAction(PlayerAction.TURN_LEFT);
-          } else{
-            testSecondPlayer.addAction(PlayerAction.TURN_LEFT);
-          }
-          break;
-        case W:
-          if (controllingPlayer == 0){
-            testFirstPlayer.addAction(PlayerAction.MOVE);
-          } else{
-            testSecondPlayer.addAction(PlayerAction.MOVE);
-          }
-          break;
-        case E:
-          if (controllingPlayer == 0){
-            testFirstPlayer.addAction(PlayerAction.TURN_RIGHT);
-          } else{
-            testSecondPlayer.addAction(PlayerAction.TURN_RIGHT);
-          }
-          break;
-        case R:
-          if (controllingPlayer == 0){
-            testFirstPlayer.addAction(PlayerAction.SHOOT);
-          } else{
-            testSecondPlayer.addAction(PlayerAction.SHOOT);
-          }
-          break;
-        case SPACE:
-          currentActionNumber  = 0;
-          controllingPlayer = 0;
-          isExecuting = true;
-          break;
-      }
-      if(!testFirstPlayer.getPlayerActions()[4].equals(PlayerAction.NONE)){
-        controllingPlayer++;
-      }
+      game.onKeyPressed(e.getCode());
     });
 
     stage.setTitle("Silver Bullet");
@@ -86,24 +47,7 @@ public class SilverBulletApplication extends Application {
   }
 
   private Parent createContent() {
-    this.root.setPrefSize(700, 700);
-
-    gameBoard = new GridBoard(9, 9);
-    root.getChildren().add(gameBoard.gridBoard);
-    testFirstPlayer = gameBoard.addPlayer(1, 1, 1);
-    testSecondPlayer = gameBoard.addPlayer(5, 5, 2);
-
-    root.getChildren().add(testFirstPlayer.playerNode);
-    root.getChildren().add(testSecondPlayer.playerNode);
-
-    root.getChildren().add(testSecondPlayer.getPlayerActionCounter());
-    root.getChildren().add(testFirstPlayer.getPlayerActionCounter());
-
-    testFirstPlayer.getPlayerActionCounter().setTranslateX(175);
-    testFirstPlayer.getPlayerActionCounter().setTranslateY(630);
-
-    testSecondPlayer.getPlayerActionCounter().setTranslateX(400);
-    testSecondPlayer.getPlayerActionCounter().setTranslateY(630);
+    this.game.setPrefSize(700, 700);
 
     timer = new AnimationTimer() {
       @Override
@@ -114,27 +58,11 @@ public class SilverBulletApplication extends Application {
 
     timer.start();
 
-    return this.root;
+    return this.game;
   }
 
   public void update(){
-    if(isExecuting){
-      if(t <= 0){
-        t = .4;
-        testFirstPlayer.takeAction(currentActionNumber);
-        testSecondPlayer.takeAction(currentActionNumber);
-        currentActionNumber++;
-        if(currentActionNumber>4){
-          isExecuting = false;
-          testFirstPlayer.getPlayerActionCounter().clearActions();
-          testSecondPlayer.getPlayerActionCounter().clearActions();
-          controllingPlayer = 0;
-        }
-      } else {
-        t -= 0.016;
-      }
-    }
-
+    game.boardUpdate();
   }
 
 
