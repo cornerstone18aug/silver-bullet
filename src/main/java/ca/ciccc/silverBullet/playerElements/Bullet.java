e7cd87b6405f5acb4f8b74a4d798667b65bc77c6
Enrique92 <enrique.alcaide92@gmail.com>
2018-11-24 13:27:33 -0800

    If you shoot the other player, we remove that player

--- src/main/java/ca/ciccc/silverBullet/playerElements/Bullet.java
+++ src/main/java/ca/ciccc/silverBullet/playerElements/Bullet.java
@@ -5,6 +5,7 @@ import ca.ciccc.silverBullet.enums.gameplay.Directions;
 import ca.ciccc.silverBullet.gameBoard.GridBoard;
 import ca.ciccc.silverBullet.gameBoard.Move;
 import ca.ciccc.silverBullet.gridNodes.GridNode;
+import javafx.animation.AnimationTimer;
 import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.scene.paint.ImagePattern;
@@ -13,15 +14,27 @@ import javafx.util.Duration;
 
 public class Bullet extends Rectangle {
 
+  AnimationTimer timer;
+  Player playerShooting;
+
   public Bullet(Move startPosition, Move endPosition, Player player) {
-    super(25, 25, 50, 50);
-    GridNode startNode = GridBoard.instance
-        .getNodeFromGrid(startPosition.getMoveX(), startPosition.getMoveY());
-    GridNode endNode = GridBoard.instance
-        .getNodeFromGrid(endPosition.getMoveX(), endPosition.getMoveY());
-    setTranslateX(startNode.getScreenX());
-    setTranslateY(startNode.getScreenY() - 50);
-    shootMovement(startNode, endNode, player);
+      super(25, 25, 50, 50);
+      playerShooting = player;
+
+      timer = new AnimationTimer() {
+        @Override
+        public void handle(long l) {
+          checkOverlap();
+        }
+      };
+
+      timer.start();
+
+      GridNode startNode = GridBoard.instance.getNodeFromGrid(startPosition.getMoveX(), startPosition.getMoveY());
+      GridNode endNode = GridBoard.instance.getNodeFromGrid(endPosition.getMoveX(), endPosition.getMoveY());
+      setTranslateX(startNode.getScreenX());
+      setTranslateY(startNode.getScreenY() - 50);
+      shootMovement(startNode, endNode, player);
   }
 
   public void shootMovement(GridNode startPos, GridNode endPos, Player player) {
@@ -62,6 +75,15 @@ public class Bullet extends Rectangle {
     transition.setToY(endPos.getScreenY() - 50);
     transition.setNode(this);
     transition.play();
+  }
+
+  public void checkOverlap(){
+    for(Player p : GridBoard.instance.players)
+      if(!p.equals(playerShooting) && getBoundsInParent().intersects(p.getPlayerNode().getBoundsInParent())){
+        p.Die();
+        timer.stop();
+        onBulletStop();
+      }
   }
 
   public void onBulletStop() {
