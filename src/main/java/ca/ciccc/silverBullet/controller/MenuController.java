532c9e8694c35479905a0a37262dc670a625ad6c
nator333 <m.nakamata35@gmail.com>
2018-11-24 12:45:37 -0800

    Add Controllers and fxmls

---
+++ src/main/java/ca/ciccc/silverBullet/controller/MenuController.java
@@ -0,0 +1,85 @@
+package ca.ciccc.silverBullet.controller;
+
+import ca.ciccc.silverBullet.SilverBulletApp;
+import ca.ciccc.silverBullet.utils.ModalUtil;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+
+/**
+ * MenuController
+ * @author Masa
+ */
+public class MenuController {
+  private static MenuController instance;
+  private static Scene SCENE;
+
+  static {
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    try {
+      fxmlLoader.load(
+          MenuController.class.getModule().getResourceAsStream("fxml/menu.fxml")
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Parent parent = fxmlLoader.getRoot();
+    Scene scene = new Scene(parent);
+    scene.setFill(Color.TRANSPARENT);
+    SCENE = scene;
+    instance = fxmlLoader.getController();
+  }
+
+  /**
+   * Return singleton instance
+   * @return instance
+   */
+  public static MenuController getInstance() {
+    synchronized (MenuController.class) {
+      if (instance == null) {
+        instance = new MenuController();
+      }
+      return instance;
+    }
+  }
+
+  public void show() {
+    SilverBulletApp.primaryStage.setScene(SCENE);
+  }
+
+  @FXML
+  public void OnStartClicked() {
+    SettingsController.getInstance().show();
+  }
+
+  @FXML
+  public void OnHowToPlayClicked() {
+    HowToPlayController.getInstance().show();
+  }
+
+  @FXML
+  public void OnQuitClicked() {
+    ModalUtil.confirm("QUIT",
+        "Did you really want to quit?",
+        e -> System.exit(1));
+  }
+
+  @FXML
+  public void OnEntered() {
+    System.out.println("Entered");
+  }
+
+  @FXML
+  public void OnPressed() {
+    System.out.println("Pressed");
+  }
+
+  @FXML
+  public void OnExited() {
+    System.out.println("Exited");
+  }
+
+}
---
+++ src/main/java/ca/ciccc/silverBullet/controller/SettingsController.java
@@ -0,0 +1,77 @@
+package ca.ciccc.silverBullet.controller;
+
+import ca.ciccc.silverBullet.SilverBulletApp;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+
+/**
+ * HowToPlayController
+ * @author Masa
+ */
+public class SettingsController {
+  private static SettingsController instance;
+  private static Scene SCENE;
+
+  static {
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    try {
+      fxmlLoader.load(
+          SettingsController.class.getModule().getResourceAsStream("fxml/settings.fxml")
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Parent parent = fxmlLoader.getRoot();
+    Scene scene = new Scene(parent);
+    scene.setFill(Color.TRANSPARENT);
+    SCENE = scene;
+    instance = fxmlLoader.getController();
+  }
+
+  /**
+   * Return singleton instance
+   * @return instance
+   */
+  public static SettingsController getInstance() {
+    synchronized (SettingsController.class) {
+      if (instance == null) {
+        instance = new SettingsController();
+      }
+      return instance;
+    }
+  }
+
+  public void show() {
+    SilverBulletApp.primaryStage.setScene(SCENE);
+  }
+
+  @FXML
+  public void onStartClicked() {
+    GameController.getInstance().show();
+  }
+
+  @FXML
+  public void onBackToMenuClicked() {
+    MenuController.getInstance().show();
+  }
+
+  @FXML
+  public void OnEntered() {
+    System.out.println("Entered");
+  }
+
+  @FXML
+  public void OnPressed() {
+    System.out.println("Pressed");
+  }
+
+  @FXML
+  public void OnExited() {
+    System.out.println("Exited");
+  }
+
+}
