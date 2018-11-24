c323d6ff64559c417f0b879169760bc69e5caf53
Unknown <juanjvalenciagd@gmail.com>
2018-11-24 13:06:45 -0800

    Re-fixed the grid.

--- src/main/java/ca/ciccc/silverBullet/gameBoard/GridBoard.java
+++ src/main/java/ca/ciccc/silverBullet/gameBoard/GridBoard.java
@@ -59,7 +59,7 @@ public class GridBoard {
       }
       targetNode = grid[targetY][targetX];
 
-      if (!targetNode.hasPlayer()) {
+      } if(!targetNode.hasPlayer() && targetNode.isCanMoveTo()) {
         return new Move(targetX, targetY);
       }
     }
