55faf01a5c0c6e25ae067d2ff85ddeca5ca904cc
Enrique92 <enrique.alcaide92@gmail.com>
2018-11-25 13:39:22 -0800

    Put in the ConstantUtil the main variables to access easier to them or if we have to change do it faster

--- src/main/java/ca/ciccc/silverBullet/utils/ConstUtil.java
+++ src/main/java/ca/ciccc/silverBullet/utils/ConstUtil.java
@@ -102,6 +102,41 @@ public class ConstUtil {
     }
   }
 
+  public enum GridBoardVariables implements hasIntSize {
+    BOARD_POSITION_X(182),
+    BOARD_POSITION_Y(50),
+    TILE_SIZE(60),
+    SPACE_TARGET_NODE(30),
+    TILE_CORRECTION_PLAYER(1);
+
+    int size;
+
+    GridBoardVariables(int size) {
+      this.size = size;
+    }
+
+    public int get(){
+      return this.size;
+    }
+  }
+
+    public enum BulletVariables implements hasIntSize {
+      SHOOT_START_POS_X(45),
+      SHOOT_END_POS_X(45),
+      SHOOT_START_POS_Y(50),
+      SHOOT_END_POS_Y(50);
+
+      int size;
+
+      BulletVariables(int size) {
+        this.size = size;
+      }
+
+    public int get(){
+      return this.size;
+    }
+  }
+
   public static final String APP_NAME = "SILVER BULLET";
   public static final String TITLE_FONT = "Times New Roman";
   public static final String MENU_FONT = "Times New Roman";
